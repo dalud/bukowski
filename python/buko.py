f7e74cae18aafb35c84aea1de3b6a5e74e022d65
dalud <jaakko.niska@gmail.com>
2023-05-10 17:01:58 +0300

    Buk modified multiple

--- python/buko.py
+++ python/buko.py
@@ -4,6 +4,8 @@ from searchInFiles_IF import Searcher
 from speak_IF import Mouth
 from nltk_IF import SubjectParser
 from random import random
+import asyncio
+from delayedPrint_IF import DelayedPrint
 
 flush = sys.stdout.flush
 
@@ -12,9 +14,10 @@ mouth = Mouth()
 sb = SubjectParser()
 dir_path = r'/home/pi/bukowski/works'
 searcher = Searcher(dir_path)
+printer = DelayedPrint(.05)
 played = []
-affirm = ['yes', 'yeah', 'ah', 'mm', 'sure', 'well', 'eh']
-decline = ['no', "I don't think so", "sorry, no", "probably not", 'negative']
+affirm = ['yes', 'yeah', 'ah', 'sure', 'well', 'eh']
+decline = ['no', "I don't think so", "sorry, no", 'negative']
 apologize = ["sorry?", "excuse me?", "huh?", "what?", "what do you mean?", "I didn't get that"]
 cls = 10
 
@@ -23,31 +26,32 @@ mouth.speak("Alright, I'm on.")
 while True:
     print('\n'*cls)
     flush()
+    #TODO: if subprocess speaking DO Not get new subject
     cue = ear.listen(True)
     subject = sb.parse(cue)
     #print(subject)
     flush()
     if subject:
         for s in subject:
-        #with subject[0] as s:
-        #s = subject[0]
             if not  s == "huh":
                 #print(s)
                 flush()
                 #mouth.speak(s+"?")
                 reply = searcher.find(s, played)
                 if reply and not reply in played:
+                    mouth.speak(s+"?")
                     mouth.speak(affirm[(int)(random()*len(affirm))])
                     print('\n'*cls)
                     flush()
-                    print(reply)
-                    flush()
-                    mouth.speak(reply)
+                    mouth.speakAsync(reply)
                     played.append(reply)
+                    printer.print(reply)
+                    flush()
                     #print("covered:" +str(len(played)))
                     flush()
                     if len(played) > 50: played.clear()
                     break
                 if subject.index(s) == len(subject)-1:
+                    mouth.speak(s+"?")
                     mouth.speak(decline[(int)(random()*len(decline))])
     else: mouth.speak(apologize[(int)(random()*len(apologize))])
--- python/speak_IF.py
+++ python/speak_IF.py
@@ -1,6 +1,11 @@
 import os
+from subprocess import Popen
 
 class Mouth:
     #def __init__(self):
     def speak(self, sentence):
         os.system('espeak -p 0 -a 100 -s 105 -v english-us "{0}"'.format(sentence))
+
+    def speakAsync(self, sentence):
+        #os.system('espeak -p 0 -a 100 -s 105 -v english-us "{0}"'.format(sentence))
+        Popen('espeak -p 0 -a 100 -s 84 -v english-us "{0}"'.format(sentence), shell=True)    
